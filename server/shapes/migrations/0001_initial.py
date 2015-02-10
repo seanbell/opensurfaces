3e52f20d44ce5303fa6d937a4e0f7c4e356707f1
Sean Bell <sbell@cs.cornell.edu>
2015-02-09 21:19:27 -0500

    working on migrations

--- server/shapes/migrations/0001_initial.py
+++ server/shapes/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("normals", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'ShapeName'
         db.create_table(u'shapes_shapename', (
