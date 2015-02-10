6df3d3dfe16d254488d7332567019e08dfe3de2b
Sean Bell <sbell@cs.cornell.edu>
2015-02-09 21:20:16 -0500

    working on migrations

--- server/normals/migrations/0001_initial.py
+++ server/normals/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("shapes", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'ShapeRectifiedNormalLabel'
         db.create_table(u'normals_shaperectifiednormallabel', (
